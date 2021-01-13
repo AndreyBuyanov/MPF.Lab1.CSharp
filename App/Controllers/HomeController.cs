18f7442d105f0d1f8580bf64313e582a574d3a9d
ANDreew1987 <andreew.12.87@gmail.com>
2021-01-13 14:02:47 +0700

    First commit

---
+++ App/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App.Models;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private bool showControls = true;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string show, string hide)
+        {
+            if (show != null)
+            {
+                showControls = true;
+            }
+            else if (hide != null)
+            {
+                showControls = false;
+            }
+            return View(showControls);
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(showControls);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
